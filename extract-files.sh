f43dd6f8a0fbb5c59e8d2865602c535b178463ee
razorloves <razorloves@gmail.com>
2016-08-10 12:36:49 -0700

    shamu: switch to new generic-extract utils
    Change-Id: Ibffd5447900030dbdefea4c9caf49c977efc3df8

--- extract-files.sh
+++ extract-files.sh
@@ -1,172 +1,56 @@
-#!/bin/bash -e
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-vendor=motorola
-device=shamu
-build=mob30i
+set -e
 
-factory_url=https://dl.google.com/dl/android/aosp/shamu-mob30i-factory-21357b09.tgz
-factory_md5=21357b098c59c7eb654d4904012ce36f38fce79b
+VENDOR=motorola
+DEVICE=shamu
 
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-self_dir="$(dirname $(readlink -f $0))"
-proprietary_files=$self_dir/proprietary-blobs.txt
-device_base=$ANDROID_BUILD_TOP/vendor/$vendor/$device/proprietary
+CM_ROOT="$MY_DIR"/../../..
 
-function md5verify(){
-  local md5=$1
-  local file=$2
-  md5sum --quiet -c <( echo "$md5 $file" )
-  if [ $? -ne 0 ]; then
-    echo "$file failed MD5 hash check"
-    return 1
-  fi
-}
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
-function fetch(){
-  local url=$1
-  local md5=$2
-  local cachedir=$ANDROID_BUILD_TOP/.fetchcache # need a better location
-  local outfile=$cachedir/${url##*/}
-
-  mkdir -p $cachedir
-
-  if [ ! -f $outfile ]; then
-    wget "$url" -O "$outfile"
-  fi
-  if md5verify "$md5" "$outfile"; then
-    echo $outfile
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
   else
-    rm $outfile
-    fetch $url $md5
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
   fi
-}
+fi
 
-function checkutils() {
-  local utils="$@"
-  for util in $utils; do
-	  echo -n " * Checking for $util..."
-	  which $util 2>&1 > /dev/null
-	  if [ $? -eq 0 ]; then
-		printf " ok!\n";
-	  else
-		printf " not found!\n";
-		exit 1;
-	  fi
-  done
-}
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-function build_smali(){
-  local outdir="$1"
-  rm -rf $outdir
-  git clone https://github.com/JesusFreke/smali $outdir
-  cd $outdir
-  ./gradlew build
-  cd -
-}
+extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
 
-function oat2dex(){
-  local file="$1"
-  local tmpdir="$2"
-
-  local smali_jar=$tmpdir/smali/smali/build/libs/smali.jar
-  local baksmali_jar=$tmpdir/smali/baksmali/build/libs/baksmali.jar
-
-  if [ ! -f "$smali_jar" ]; then
-    build_smali "$tmpdir/smali"
-  fi
-
-  if [ ! -e "`dirname $file`/oat/arm/`basename $file ."${file##*.}"`.odex" ] && [ ! -e "`dirname $file`/oat/arm64/`basename $file ."${file##*.}"`.odex" ]; then
-    return 0
-  fi
-
-  mkdir -p $tmpdir/dexout
-  for x in "`dirname $file`/oat/arm/`basename $file ."${file##*.}"`.odex" "`dirname $file`/oat/arm64/`basename $file ."${file##*.}"`.odex"; do
-    [ ! -e "$x" ] && continue
-    java -jar "$baksmali_jar" -x -o "$tmpdir/dexout" -c boot.oat -d "$tmpdir/system/framework/arm64" -d "$mpdir/system/framework/arm" "$x"
-    ls -l $tmpdir/dexout | wc -l
-  done
-  java -jar "$smali_jar" "$tmpdir/dexout" -o "$tmpdir/classes.dex"
-  rm -rf "$tmpdir/dexout"
-}
-
-function unpack_factory(){
-  local factory_url=$1
-  local factory_md5=$2
-  local outdir=$3
-  local factory_file=$(fetch "$factory_url" "$factory_md5")
-  local device=$(basename $factory_file | sed 's/\(^\w\+\).*/\1/g')
-  local build=$(basename $factory_file | sed 's/^\w\+-\([a-z0-9]\+\)-.*/\1/g')
-
-  mkdir -p $outdir/extracted
-
-  tar -C "$outdir" -xvzf "$factory_file"
-
-  unzip  -d $outdir -o $outdir/$device-$build/image-$device-${build}.zip
-
-  for image in system vendor; do
-    simg2img $outdir/${image}.img $outdir/${image}.ext4.img
-	mkdir -p $outdir/${image}_mount
-
-	# I really hate using sudo here but I see no other choice
-	# Any other ideas to extract these are welcome
-	sudo mount -o loop $outdir/${image}.ext4.img $outdir/${image}_mount
-	sudo cp -r $outdir/${image}_mount $outdir/$image
-    sudo umount $outdir/${image}_mount
-    sudo rm -rf $outdir/${image}_mount
-    sudo chown -R $USER $outdir/${image}
-  done
-
-}
-
-function extract(){
-  local factory_url=$1
-  local factory_md5=$2
-  local proprietary_files=$3
-  local outbase=$4
-  local tmpdir="/tmp/extract-files/$device/$build"
-
-  mkdir -p $4
-  mkdir -p $tmpdir
-
-  unpack_factory "$factory_url" "$factory_md5" "$tmpdir"
-
-  for file in $(egrep -v '(^#|^$)' $proprietary_files); do
-      echo "Extracting $file ..."
-      oldifs=$IFS IFS=":" parsing_array=($file) IFS=$oldifs
-      srcfile=$(echo ${parsing_array[0]} | sed -e "s/^-//g")
-      destfile=${parsing_array[1]}
-      if [ -z "$destfile" ]; then
-          destfile="$srcfile"
-      fi
-      destfile=$(echo "$destfile" | sed 's|^system/||')
-      destdir=$(dirname "$destfile")
-      if [ ! -d "$outbase/$destdir" ]; then
-          mkdir -p "$outbase/$destdir"
-      fi
-
-      cp "$tmpdir/$srcfile" "$outbase/$destfile"
-
-      # Fixup xml files
-      if [[ "$outbase/$destfile" =~ .xml$ ]]; then
-          xmlheader=$(grep '^<?xml version' "$outbase/$destfile")
-          grep -v '^<?xml version' "$outbase/$destfile" > "$outbase/$destfile".temp
-          (echo "$xmlheader"; cat "$outbase/$destfile".temp ) > "$outbase/$destfile"
-          rm "$outbase/$destfile".temp
-      fi
-      if [[ "$destfile" =~ .(apk|jar)$ ]]; then
-          oat2dex "$tmpdir/$srcfile" $tmpdir
-          if [ -e "$tmpdir/classes.dex" ]; then
-              zip -gjq "$outbase/$destfile" "$tmpdir/classes.dex"
-              rm "$tmpdir/classes.dex"
-              echo "Updated $outbase/$destfile from odex files"
-          fi
-      fi
-  done
-
-}
-
-checkutils simg2img unzip md5sum sudo wget
-
-extract "$factory_url" "$factory_md5" "$proprietary_files" "$device_base"
-
-bash $self_dir/setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh
